c1bee58434ef9152b2edee600fc3189b0a65806e
Mohan Patil <patilmohan2307@gmail.com>
2026-05-25 12:31:00 +0530

    Added CORS configuration for deployement

--- Backend/src/main/java/com/ecommerce/Backend/config/WebConfig.java
+++ Backend/src/main/java/com/ecommerce/Backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.Backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:/uploads/");
+    }
+//    for deployment need this configuration.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://quickshophere.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
